a63143f91233b0bc94e01e3d1033a2020ed5e358
medantechno <admin@medantechno.com>
2021-11-24 12:11:32 +0700

    barcode

--- application/views/get_barcode.php
+++ application/views/get_barcode.php
@@ -8,10 +8,10 @@
     //echo $barang->nama_barang;
 
     for ($i=0; $i < $value['jumlah_barcode'] ; $i++) { 
-      echo "<img src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$barang->nama_barang&print=true'> &nbsp;&nbsp;&nbsp;&nbsp;";
+      echo "<img src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=20&text=$barang->nama_barang&print=true' width='200px'> &nbsp;&nbsp;&nbsp;&nbsp;";
     }
     
-    echo"<br><hr><br>";
+    echo"<br><br>";
 
   }
 ?>
